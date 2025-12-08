7a67e9c0a7bbbb7a1c195e3fd314c8516ccc7de8
Leo <hc@adrop.io>
2025-12-08 11:45:50 +0900

    1.7.2-alpha.0

---
+++ AdropAdsBackfill.xcframework/ios-arm64_x86_64-simulator/AdropAdsBackfill.framework/Headers/AdropAdsBackfill.h
@@ -0,0 +1,14 @@
+//
+//  AdropAdsBackfill.h
+//  AdropAdsBackfill
+//
+//  Created by Adrop
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdropAdsBackfill.
+FOUNDATION_EXPORT double AdropAdsBackfillVersionNumber;
+
+//! Project version string for AdropAdsBackfill.
+FOUNDATION_EXPORT const unsigned char AdropAdsBackfillVersionString[];
