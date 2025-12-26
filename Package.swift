a66aebeac83add36322cec10e04a092dbe621d57
Leo <hc@adrop.io>
2025-12-26 14:07:31 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/OpenRhapsody/adrop-ads-pod.git", "1.7.0"..<"1.8.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", "12.2.0"..<"13.0.0"),
+        .package(url: "https://github.com/OpenRhapsody/adrop-ads-pod.git", from: "1.7.3"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "12.2.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git", from: "3.1.0"),
 
         // AdMob Mediation Adapters
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-pangle.git", from: "6.0.0"),
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-meta.git", from: "6.0.0"),
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-unity.git", from: "4.0.0"),
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-inmobi.git", from: "10.0.0"),
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-liftoffmonetize.git", from: "7.0.0"),
-        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-applovin.git", from: "13.0.0")
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-pangle.git", branch: "main"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-unity.git", branch: "main"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-inmobi.git", branch: "main"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-liftoffmonetize.git", branch: "main"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-applovin.git", branch: "main")
     ],
     targets: [
         .binaryTarget(
